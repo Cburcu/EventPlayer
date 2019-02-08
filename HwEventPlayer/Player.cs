5b959e4051e2c789c8eabdfdd1e48bfd70212202
Cemile Burcu Atay <Burcu.Atay@d-teknoloji.com.tr>
2019-02-08 09:36:39 +0300

    Add project files.

---
+++ HwEventPlayer/Player.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwEventPlayer
+{
+    public delegate void PointControl(object source, PointEventArgs args);
+
+    class Player
+    {
+        //public event PlayerLevelChangedDelegate LevelDecreased;
+        public event PointControl PointControlEvent;
+        public event EventHandler<LevelEventArgs> LevelControlEvent;
+        
+        public string Nickname { get; set; }
+
+        public int Points
+        {
+            get { return _points; }
+            set
+            {
+                _points = value;
+                if (PointControlEvent != null && value % 100 == 0)
+                {
+                    Level += 1;
+                    PointControlEvent(this, new PointEventArgs(){ CurrentValue = value});
+                }
+            }
+        }
+
+        private int _level;
+        private int _points;
+
+        public int Level
+        {
+            get
+            {
+                return _level;
+            }
+            set
+            {
+                int oldLevet = _level;
+                _level = value;
+
+                if ((value - oldLevet) == 1 && LevelControlEvent != null)
+                {
+                    Console.WriteLine("Level up {0} => {1}", value, Level);
+                    LevelControlEvent(this, new LevelEventArgs(){ CurrentLevel =_level, OldLevel = oldLevet });
+                }
+            }
+        }
+
+        public void LevelUp(int experience)
+        {
+            Points += experience;
+            Console.WriteLine("New point = {0}", Points);
+        }
+    }
+
+    public class PointEventArgs
+    {
+        public int CurrentValue { get; set; }
+    }
+
+    public class LevelEventArgs:EventArgs
+    {
+        public int CurrentLevel { get; set; }
+        public int OldLevel { get; set; }
+    }
+}
---
+++ HwEventPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwEventPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player emcey = new Player();
+            emcey.Nickname = "emcey";
+
+            emcey.PointControlEvent += (o , e) =>
+            {
+                Console.WriteLine("levelup");
+                Console.WriteLine(e.CurrentValue);
+            }
+            ;
+            
+            emcey.LevelControlEvent += Player_LevelControlEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                emcey.LevelUp(25);
+                Console.WriteLine("{0} Points = {1}  Level = {2}", emcey.Nickname, emcey.Points, emcey.Level);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void Player_LevelControlEvent(object ob, LevelEventArgs args)
+        {
+            Console.WriteLine("Congratulation");
+            Console.WriteLine("old Value= {0} current value = {1}", args.OldLevel, args.CurrentLevel);
+
+        }
+
+    }
+}
